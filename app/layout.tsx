008f7088508841ca4ee2ec4a42ff8a170a025877
Will <47335043+williamfedele@users.noreply.github.com>
2025-01-18 01:49:37 -0500

    feat: day view

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,10 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Rajdhani } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const rajdhani = Rajdhani({
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
 });
 
 export const metadata: Metadata = {
@@ -25,7 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased dark`}
+        className={`${rajdhani.className} antialiased dark p-4 font-semibold text-lg`}
       >
         {children}
       </body>
--- components/calendar.tsx
+++ components/calendar.tsx
@@ -4,7 +4,7 @@ import { useState } from "react";
 export const Calendar = () => {
   const [view, setView] = useState("day");
   return (
-    <div>
+    <div className="space-y-4">
       <div className="space-x-4">
         <button onClick={() => setView("day")}>Days</button>
         <button onClick={() => setView("month")}>Month</button>
@@ -18,12 +18,37 @@ export const Calendar = () => {
 };
 
 const DayView = () => {
+
+  const getCurrentDayOfYear = () => {
+    const now = new Date();
+    const dayOne = new Date(now.getFullYear(), 0, 0);
+    const diff = now - dayOne;
+    const oneDayMs = 24 * 60 * 60 * 1000;
+    return Math.floor(diff / oneDayMs);
+  };
+
+  const currentDay = getCurrentDayOfYear();
+
   return (
-    <div className="space-x-1">
-      <p>test</p>
-      {Array.from({ length: 365 }, (_, index) => (
-        <span key={index + 1}>. </span>
-      ))}
+    <div className="w-full flex flex-wrap gap-4">
+      {[...Array(365)].map((_, index) => {
+        const dayNumber = index + 1;
+        return (
+          <div
+            key={index}
+            className={`
+              w-1 h-1 rounded-full transition-colors
+              ${
+                dayNumber === currentDay
+                  ? "bg-foreground"
+                  : dayNumber < currentDay
+                    ? "bg-primary-foreground"
+                    : "bg-accent"
+              }
+            `}
+          />
+        );
+      })}
     </div>
   );
 };
